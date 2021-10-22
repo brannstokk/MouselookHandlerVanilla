de345485f652d85ade42385ecca26666df13ad52
jlucas <jlucas@ash.106.net>
2021-10-22 03:34:38 -0400

    Add frame and OnUpdate loop to handle spell targeting
    Disable mouselook when targeting a spell.

--- MouselookHandlerVanilla.lua
+++ MouselookHandlerVanilla.lua
@@ -1,12 +1,42 @@
+-- In 1.10, a Mouselook mode was added to the UI.
+
 MouselookHandlerVanilla = {}
+local shouldMouselook = false
+local handlerFrame = CreateFrame("Frame")
 
 function MouselookHandlerVanilla:toggle()
-   if IsMouselooking() == 1 then
-      MouselookStop()
-      SetBinding("BUTTON2", "TURNORACTION")
-    else
-      MouselookStart()
-      SetBinding("BUTTON2", "TOGGLEAUTORUN")
-    end
+	-- https://wowwiki-archive.fandom.com/wiki/API_IsMouselooking
+	-- https://vanilla-wow-archive.fandom.com/wiki/API_CursorHasItem
+	-- Should we DropCursorItem()
+	if IsMouselooking() == 1 then
+		MouselookHandlerVanilla:disable()
+	else
+		MouselookHandlerVanilla:enable()
+	end
 end
+
+function MouselookHandlerVanilla:disable(defer)
+	MouselookStop()
+    if not defer then
+       shouldMouselook = false
+    end
+	SetBinding("BUTTON2", "TURNORACTION")
+end
+
+function MouselookHandlerVanilla:enable(defer)
+	MouselookStart()
+    if not defer then
+       shouldMouselook = true
+    end
+	SetBinding("BUTTON2", "TOGGLEAUTORUN")
+end
+
+-- Disable mouselook when targeting a spell
+function handlerFrame:onUpdate(...)
+   if SpellIsTargeting() and shouldMouselook then
+      MouselookHandlerVanilla:disable()
+   end
+end
+
+handlerFrame:SetScript("OnUpdate", handlerFrame.onUpdate)
 
